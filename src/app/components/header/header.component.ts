53a30eed2ec3396b1abc75b00a49b6cc2dc0cb9e
Gustavo Vinicius <gus.tec.dev@gmail.com>
2026-02-24 13:07:54 -0300

    (feat): Adding a profile menu to the header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,16 +1,31 @@
 import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
+import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
 
 @Component({
   selector: 'app-header',
-  standalone: true,
+  standalone: true, 
+  imports: [MatIconModule, MatMenuModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
   private router = inject(Router);
   private authService = inject(AuthService);
+  name: string | null = null;
+
+  ngOnInit() {
+    this.getUserName();
+  }
+
+  getUserName() {
+    this.authService.getCurrentUser()
+    .subscribe(user => {
+      this.name = user.name;
+    });
+  }
 
   goToLogin() {
     this.router.navigate(['']);
